921bc1aec75ca5925f94ccc7f5c4c96bc53395f7
한규창 <61011675+ByMaro@users.noreply.github.com>
2022-06-19 18:40:53 +0900

    Update reco_for_scoreTemp.java

--- smhrd_project/src/main/java/com/model/reco_for_scoreTemp.java
+++ smhrd_project/src/main/java/com/model/reco_for_scoreTemp.java
@@ -9,12 +9,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter
 @Getter
-
+//추천 알고리즘에 사용할 가중치 저장 클래스
 public class reco_for_scoreTemp {
-	  private double level_score = 1;
-	  private double favor_part_score = 1;
-	  private double hate_part_score = 1;
-	  private double yesterday_part_score = 1;
-	  private double eq_score = 1;
+	  private double level_score = 1;	//난이도
+ 	  private double favor_part_score = 1;	//선호 운동 부위
+	  private double hate_part_score = 1;	//비선호 운동 부위
+	  private double yesterday_part_score = 1; //이전 운동 부위
+	  private double eq_score = 1;	//선호 장비
 
 }
