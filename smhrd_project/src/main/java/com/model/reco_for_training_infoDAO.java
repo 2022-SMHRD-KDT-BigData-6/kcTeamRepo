2ab8a2e944fb532f12a039f05dce879d64909b23
한규창 <61011675+ByMaro@users.noreply.github.com>
2022-06-19 18:43:21 +0900

    Update reco_for_training_infoDAO.java

--- smhrd_project/src/main/java/com/model/reco_for_training_infoDAO.java
+++ smhrd_project/src/main/java/com/model/reco_for_training_infoDAO.java
@@ -11,7 +11,8 @@ public class reco_for_training_infoDAO {
    SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSessionFactory();
    SqlSession session = null;
    int cnt = 0;
-   
+
+   //운동정보 + 가중치 최종 값 저장 vo 초기화
    public List<reco_for_training_infovo> reco_training_score_init(){
       session = sqlSessionFactory.openSession(true);
       List<reco_for_training_infovo> vo = null;
@@ -26,6 +27,7 @@ public class reco_for_training_infoDAO {
       
    }
    
+   //1일전 운동 부위에 대한 정보 호출
    public List<String> reco_one_yesterday_training_parts(String user_id){
       session = sqlSessionFactory.openSession(true);
       List<String> vo = null;
@@ -41,7 +43,7 @@ public class reco_for_training_infoDAO {
       
    }
    
-   
+   //2일전 운동 부위에 대한 종보 호출
    public List<String> reco_two_yesterday_training_parts(String user_id){
       session = sqlSessionFactory.openSession(true);
       List<String> vo = null;
@@ -57,6 +59,7 @@ public class reco_for_training_infoDAO {
       
    }
    
+   //오늘 추천 받은 운동에 대한 정보들을 호출
    public List<today_user_trainingVO> reco_today_training_parts(String user_id){
       session = sqlSessionFactory.openSession(true);
       List<today_user_trainingVO> vo = null;
