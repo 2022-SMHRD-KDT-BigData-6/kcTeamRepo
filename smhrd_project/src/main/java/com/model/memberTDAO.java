2caff9044b887e2e8f5b5e0e96e24f5491a863c9
한규창 <61011675+ByMaro@users.noreply.github.com>
2022-06-19 18:36:43 +0900

    Update memberTDAO.java

--- smhrd_project/src/main/java/com/model/memberTDAO.java
+++ smhrd_project/src/main/java/com/model/memberTDAO.java
@@ -11,6 +11,8 @@ public class memberTDAO {
 	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSessionFactory();
 	SqlSession session = null;
 	
+	
+	//사용자의 모든 운동명 목록 조회(이미지 출력에 사용)
 	public List<String> user_info_img(){
 		SqlSession session = sqlSessionFactory.openSession(true);
 		List<String> list= session.selectList("usertselect");
@@ -21,7 +23,7 @@ public class memberTDAO {
 	} 
 	
 	
-	
+	//사용자의 모든 운동 정보 조회
 	public List<usertlVO> user_info_all(String user_id, String date){
 		SqlSession session = sqlSessionFactory.openSession(true);
 		usertlVO utlvo = new usertlVO();
